0abcba86f335b60964c6abded864f6532ae324e9
Hex-byte-glich-game <manzground@gmail.com>
2025-03-16 22:40:01 +0700

    Add files via upload

---
+++ SDK.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+// AttackOnline 2.0 (HexagonLogger SDK) SDK
+
+
+#include "SDK/AK2_Basic.hpp"
+
+#include "SDK/AK2_Editor_classes.hpp"
+#include "SDK/AK2_Core_classes.hpp"
+#include "SDK/AK2_Engine_classes.hpp"
+#include "SDK/AK2_WinDrv_classes.hpp"
+#include "SDK/AK2_Fire_classes.hpp"
+#include "SDK/AK2_ALAudio_classes.hpp"
+#include "SDK/AK2_D3D9Drv_classes.hpp"
+#include "SDK/AK2_IpDrv_classes.hpp"
+#include "SDK/AK2_XInterface_classes.hpp"
+#include "SDK/AK2_UWeb_classes.hpp"
+#include "SDK/AK2_KFXVehicles_classes.hpp"
+#include "SDK/AK2_KFXGame_classes.hpp"
+#include "SDK/AK2_UnrealGame_classes.hpp"
+#include "SDK/AK2_KFXClient_classes.hpp"
+#include "SDK/AK2_KFXGUI_classes.hpp"
+#include "SDK/AK2_Window_classes.hpp"
+#include "SDK/AK2_Gameplay_classes.hpp"
+#include "SDK/AK2_KFXEffects_classes.hpp"
+#include "SDK/AK2_KFXNAEffects_classes.hpp"
+#include "SDK/AK2_KFXStyle_classes.hpp"
+#include "SDK/AK2_KFXProto_classes.hpp"
+#include "SDK/AK2_KFXWeapons_classes.hpp"
+#include "SDK/AK2_KFXGame2_classes.hpp"
+#include "SDK/AK2_KFXProtoRecieve_classes.hpp"
